b3fdae2f27b570e6756e977e05fb66d0c09c6d6f
Jorgestarx <jorginogomezito@gmail.com>
2026-05-11 17:25:53 -0500

    feat: agregar módulo de excepciones personalizadas

---
+++ #Excepciones.py
@@ -0,0 +1,19 @@
+#Excepciones
+
+class nombreinvalidoerror(Exception):
+    pass
+
+class dniinvalidoerror(Exception):
+    pass
+
+class correoivalidoerror(Exception):
+    pass
+
+class telefónoinvalidoerror(Exception):
+    pass
+
+class tiempoinvalidoerror(Exception):
+    pass
+
+class servicioinvalidoerror(Exception):
+    pass
